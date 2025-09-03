6e2aa0d5648b42280de03bd5e50e6a66d2b0569c
nathanscd <nathansec26@gmail.com>
2025-09-03 11:27:04 -0300

    change index.html

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from extensions import db
+from models import User, Activity, Event
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'sua_chave_secreta'
 
-# Inicializa extensões
 db.init_app(app)
-
-# Importa rotas e modelos depois do init_app
-from models import User, Activity, Event
-import routes  # arquivo onde você pode separar as rotas
+import routes 
